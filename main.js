0e4ca82ab20e978e9c079add6e7cb4a8262f8fe6
Matheus André <92062874+matheusandre1@users.noreply.github.com>
2022-05-07 01:10:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavonScroll()
+  showBackToTopButtonOnScroll()
+  activeMenuAtCurrentSection(home)
+  activeMenuAtCurrentSection(services)
+  activeMenuAtCurrentSection(about)
+  activeMenuAtCurrentSection(contact)
+}
+function activeMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção pessou da linha//
+  // quais dados vou precisar ? //
+  // o top da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha
+  const sectionTopReachOrPassedTargetline = targetline >= sectionTop
+
+  // verificar se a base está abaixod da linha imaginaria
+  // quais dados vou precisar //
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+
+  const menuElement = document.querySelector(`.menu a[hred*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavonScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+#home, 
+#home img,
+ #home .stats, 
+ #services,
+ #services header,
+ #services .card,
+ #about,
+ #about header,
+ #about .content`)
